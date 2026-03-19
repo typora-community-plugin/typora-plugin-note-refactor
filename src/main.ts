47ef8d4aeb9e36970c77437568a8c91abcdb0aea
plylrnsdy <plylrnsdy@163.com>
2026-03-20 01:19:37 +0800

    feat: extract note with manual renaming

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { fs, I18n, path, Plugin } from '@typora-community-plugin/core'
+import { fs, I18n, openInputBox, path, Plugin } from '@typora-community-plugin/core'
 import { editor, File, isInputComponent } from 'typora'
 
 
@@ -6,22 +6,37 @@ export default class extends Plugin {
 
   i18n = new I18n({
     resources: {
-      'en': { command: 'Extract selection to new note - first line as file name' },
-      'zh-cn': { command: '提取选中内容到新笔记 - 第一行作为文件名' },
+      'en': {
+        extract: 'Extract selection to new note - first line as file name',
+        extractRename: 'Extract selection to new note - manual rename it',
+        placeholder: 'Input the new note\'s name',
+      },
+      'zh-cn': {
+        extract: '提取选中内容到新笔记 - 第一行作为文件名',
+        extractRename: '提取选中内容到新笔记 - 手动重命名',
+        placeholder: '请输入笔记名称',
+      },
     }
   })
 
   onload() {
 
     this.registerCommand({
-      id: 'note-refactor',
-      title: this.i18n.t.command,
+      id: 'note-refactor:rename',
+      title: this.i18n.t.extractRename,
       scope: 'editor',
-      callback: () => this.extractSelection(),
+      callback: () => this.extractSelection(true),
+    })
+
+    this.registerCommand({
+      id: 'note-refactor',
+      title: this.i18n.t.extract,
+      scope: 'editor',
+      callback: () => this.extractSelection(false),
     })
   }
 
-  async extractSelection() {
+  async extractSelection(useManualRename: boolean) {
     if (isInputComponent(document.activeElement)) return
 
     const range = editor.selection.getRangy()
@@ -30,8 +45,13 @@ export default class extends Plugin {
     File.copy()
     const md = await navigator.clipboard.readText()
 
-    const filename = md.split('\n').at(0)!
-      .replace(/[\\\/:*?"<>|\[\]#]/g, '')
+    let filename = useManualRename
+      ? await openInputBox({
+        placeholder: this.i18n.t.placeholder,
+      }) ?? 'Untitled'
+      : md.split('\n').at(0)!
+        .replace(/[\\\/:*?"<>|\[\]#]/g, '')
+
     const notepath = path.join(path.dirname(this.app.workspace.activeFile), filename + '.md')
 
     fs.writeText(notepath, md)
